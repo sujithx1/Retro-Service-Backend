108672229319753510987da832d377b04f1a6409
sujith <syjithkpy@gmail.com>
2025-04-03 23:10:39 +0530

    some changes added in checkout

--- src/use-cases/store/checkout/postcheckout.ts
+++ src/use-cases/store/checkout/postcheckout.ts
@@ -7,77 +7,85 @@ import { IwalletRepositories } from "../../../interfaces/repositories/wallet/Iwa
 import { CustomError } from "../../../utils/errors/custom.errors";
 import { AppError } from "../../../utils/errors/error.enum";
 
-export class CheckOut_useCase{
-    constructor(
-        private checkoutrepositories:IcheckoutRepositories,
-        private cartrepositoires:Icartrepositories,
-        private transactionrepositories:ItransactionRepositories,
-        private walletrepositories:IwalletRepositories
-    ) {
-        
+export class CheckOut_useCase {
+  constructor(
+    private checkoutrepositories: IcheckoutRepositories,
+    private cartrepositoires: Icartrepositories,
+    private transactionrepositories: ItransactionRepositories,
+    private walletrepositories: IwalletRepositories
+  ) {}
+  async execute(
+    cartId: string,
+    total: number,
+    paymentMethod: "cod" | "razorpay" | "wallet",
+    transactionId: string
+  ): Promise<CheckoutEntities> {
+    const cart = await this.cartrepositoires.findById(cartId);
+    if (!cart)
+      throw new CustomError("Cart not Found", 404, AppError.ResourceNotFound);
+
+    const products = cart.products;
+
+    const checkoutEntity = new CheckoutEntities(
+      "",
+      cart.userId.toString(),
+      cart.storeId,
+      {
+        products,
+      },
+
+      total,
+      paymentMethod,
+      "completed",
+      "pending",
+      transactionId
+    );
+    console.log(checkoutEntity);
+
+    const checkout = await this.checkoutrepositories.create(checkoutEntity);
+    const storeWallet = await this.walletrepositories.findByStoreId(
+      cart.storeId.toString()
+    );
+    if (!storeWallet)
+      throw new CustomError(
+        "storeWallet not Found",
+        404,
+        AppError.ResourceNotFound
+      );
+
+    storeWallet.balance = total;
+    const updatestoreWallet =
+      await this.walletrepositories.findByuserIdandUpdate(storeWallet);
+
+    if (!updatestoreWallet)
+      throw new CustomError(
+        "storeWallet not updated",
+        500,
+        AppError.ServerError
+      );
+
+    const usertransaction = new TransactionEntities(
+      "",
+      checkout.userId.toString(),
+      "purchase",
+      Number(total),
+      "complete",
+      paymentMethod.trim() as "razorpay" | "wallet" | "cod",
+      "product"
+    );
+
+    await this.transactionrepositories.create(usertransaction);
+    if (paymentMethod == "wallet") {
+      const userwallet = await this.walletrepositories.findByuserId(
+        cart.userId.toString()
+      );
+      if (userwallet) {
+        userwallet.balance = -total;
+      }
     }
-    async execute(cartId:string,total:number,paymentMethod:"cod"|"razorpay"|"wallet",transactionId:string):Promise<CheckoutEntities>{
 
-        const cart=await this.cartrepositoires.findById(cartId)
-        if(!cart) throw new CustomError("Cart not Found",404,AppError.ResourceNotFound);
+    await this.cartrepositoires.findByIdAndDelete(cart.id);
 
-
-        const products=cart.products
-        
-        const checkoutEntity=new CheckoutEntities(
-            "",
-            cart.userId.toString(),
-            cart.storeId,
-            {
-                products
-
-            },
-          
-            total,
-            paymentMethod,
-            "completed",
-            "pending",
-            transactionId
-
-
-        )
-        console.log(checkoutEntity); 
-        
-        const checkout=await this.checkoutrepositories.create(checkoutEntity)
-        const storeWallet=await this.walletrepositories.findByStoreId(cart.storeId.toString())
-        if(!storeWallet) throw new CustomError("storeWallet not Found",404,AppError.ResourceNotFound);
-
-        storeWallet.balance=total
-        const updatestoreWallet=await this.walletrepositories.findByuserIdandUpdate(storeWallet)
-
-        if(!updatestoreWallet) throw new CustomError("storeWallet not updated",500,AppError.ServerError);
-
-
-
-
-         const usertransaction=new TransactionEntities(
-                    "",
-                    checkout.userId.toString(),
-                    "purchase",
-                    Number(total),
-                    "complete",
-                    paymentMethod as "razorpay" | "wallet" | "cod",
-                  "product"
-        
-                   )
-        
-                   await this.transactionrepositories.create(usertransaction)
-if (paymentMethod=="wallet") {
-    const userwallet=await this.walletrepositories.findByuserId(cart.userId.toString())
-    if (userwallet) {
-     userwallet.balance=-total   
-    }
-    
-    
-}
-
-        await this.cartrepositoires.findByIdAndDelete(cart.id)
-
-        return checkout
-    }
+    return checkout;
+  }
 }
