00585a1e794bfc196a98b146730c5fdc769edbf5
sujith <syjithkpy@gmail.com>
2025-06-28 16:58:24 +0530

    removed dist

--- dist/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// dotenv.config()
-// import server from "./app";
-// const port=process.env.PORT 
-// server.listen(port,()=>console.log(`server running ${port}`)
-// )    
--- dist/utils/helper/generateStoreId.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateStoreID = void 0;
-const generateStoreID = (storeName) => {
-    const cleanedName = storeName.trim().toLowerCase().replace(/\s+/g, "");
-    const timestamp = Date.now().toString().slice(-4);
-    return `${cleanedName}${timestamp}`;
-};
-exports.generateStoreID = generateStoreID;
